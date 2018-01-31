c7d7a9f45b1ae73245f0dca31fd01ebd2126ce4f
Poline Lottin <lottinp@gmail.com>
2018-01-30 23:51:57 -0200

    NOQA: Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,19 @@
-mv tmux/ .tmux/
-mv tmux.conf .tmux.conf 
+# Create aliases
+cp aliases .aliases
+
+# Create nvim folder
+mkdir -p ~/.config/nvim/
+
+# Copy nvim files
+cp nvim/init.vim nvim/plugins.vim ~/.config/nvim/
+
+# Install plugged
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Tmux stuff
+cp tmux/ ~/.tmux
+cp tmux-theme.sh ~/.tmux-theme.sh
+cp tmux.conf ~/.tmux.conf
+
+cp zsrhc ~/.zshrc
